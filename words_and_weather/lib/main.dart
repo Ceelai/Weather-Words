3390904f78612f9b512b212f34acd1b4909536d4
Calvin <laica@sheridancollege.ca>
2020-02-16 18:13:13 -0500

    GPS Location Query Works
    Fixed Query for GPS, need to complete initial query to weather api

--- words_and_weather/lib/main.dart
+++ words_and_weather/lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -13,16 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Words and Weather',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.white,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Words and Weather Home Page'),
     );
@@ -30,14 +19,29 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
   //String _locality = '';
   //String _weather = '';
+  Position _currentPosition; 
+
 
   @override 
   initState(){
@@ -53,28 +57,27 @@ class _MyHomePageState extends State<MyHomePage> {
     return result;
   }
 
-    //get position method
+  //get position method
   Future<Position> getPosition() async{
     Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return position;
+    _currentPosition = position;
+    print(_currentPosition);
+    return _currentPosition;
   }
 
   //get weather method 
   Future<String> getData(double latitude, double longitude) async{
-    String api = 'https://www.metaweather.com/api/location/search/';
+    String api = 'https://www.metaweather.com//api/location/search/?lattlong=';
 
-   
+    
 
     //final url = '$api?lattlong=$latitude,$longitude';
-    final url = '$api?query=$placemark'
+    final url = '$api$_currentPosition.lattitude,$_currentPosition.longitude';
 
     http.Response response = await http.get(url);
 
-    if (response != 200){
-      throw Exception('error getting location for city');
-    }
-
     final locationJson = jsonDecode(response.body) as List;
+    print(locationJson.first);
     return (locationJson.first);
   }
 
@@ -82,16 +85,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override 
   Widget build(BuildContext context){
-    return Scaffold(appBar:AppBar(title: Text('Words and Weather')
-    ,)
-    , 
-    body: Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[Text('Words and Weather')
-      ]
-      ,)
-      ,)
-      );
+    return Scaffold(appBar:AppBar(title: Text('Words and Weather'),),);
+      
+    
   }
 
 }
+
